6a01bce3f84d79ff4fe0a60a2d0b883723d54697
aarcThom <thomas@aarc.io>
2024-05-29 12:32:44 -0700

    Add assignment 01

---
+++ assignments/ex1/lastname_firstname.py
@@ -0,0 +1,26 @@
+import random # you probably want to import this module for the rock, paper, scissors function
+
+# Function 01:
+def custom_greeting(greeting:str, *names:str) -> str:
+    """Write a function that returns a greeting for 0, 1, or 2 or more people."""
+
+    return "A string"
+
+
+# Function 02:
+def order_produce_prices(produce:list[str], indiv_price:list[float], qty:list[int]) -> list[str]:
+    """Write a function that takes 3 corresponding lists - produce description (eg. apple), 
+    price per unit (eg. price per apple = 0.75), and quantity (eg. I need 5 apples) - 
+    and formats them into corresponding string line items, formatted as below."""
+
+    return ["A", "list", "of", "strings"]
+
+
+# Function 03:
+def rock_paper_scissors():
+    """Plays one round of rock, paper, scissors!"""
+
+    # don't return anything
+
+
+
---
+++ assignments/ex1/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.8 (trial), 000000, 2024-05-28T21:52:55.314336
+from .pyarmor_runtime import __pyarmor__
